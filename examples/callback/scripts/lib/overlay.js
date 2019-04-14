b27d8e1a5f7ade45af6d7fc55d6ad0ed2f9356e5
KatvonRivia <katvonrivia@gmail.com>
2019-04-14 17:17:35 +0200

    simplify modal form

--- examples/callback/scripts/lib/overlay.js
+++ examples/callback/scripts/lib/overlay.js
@@ -1,87 +1,68 @@
 const closeModal = () => {
-    app.overlay.$modal.style.display = 'none';
+    app.overlay.$modal.style.display = "none";
+    emptyForm();
 };
 
-const createInputFields = (inputTypes, modalBox, options, destination) => {
-    inputTypes
-        .map(type => {
-            const input = createElement('input');
-            const span = createElement('span');
-            input.id = `${type}-input`;
-            if (destination.position) {
-                input.value = destination[type];
-            }
-            if (type === 'latitude') {
-                input.className = 'latitude'
-                if (destination.position) {
-                    input.value = destination.position.lat;
-                }
-            }
-            if (type === 'longitude') {
-                input.className = 'longitude'
-                if (destination.position) {
-                    input.value = destination.position.lng;
-                }
-            }
-            span.textContent = `Enter ${type}:`;
-            return [span, input];
-        })
-        .forEach(pair => {
-            modalBox.appendChild(pair[0]);
-            modalBox.appendChild(pair[1]);
-        });
+const prefilledForm = destination => {
+    const { position, name, description, continent } = destination;
+    if (name) $(".name").value = name;
+    if (description) $(".description").value = description;
+    if (continent) $(".continent").value = continent;
+    if (position) {
+        $(".latitude").value = position.lat;
+        $(".longitude").value = position.lng;
+    }
+};
+
+const emptyForm = () => {
+    $(".name").value = "";
+    $(".description").value = "";
+    $(".continent").value = "";
+    $(".latitude").value = "";
+    $(".longitude").value = "";
+    $(".post-button").remove();
+    $(".close-button").remove();
 };
 
 const openAddDestinationForm = (onSave, options, destination = {}) => {
-    // if (!app.auth.user) {
-    //   return;
-    // }
+    app.overlay.$modal.style.display = "flex";
+    prefilledForm(destination);
 
-    app.overlay.$modal.style.display = 'flex';
-    const modalBox = createElement('div');
-    modalBox.className = 'modal-content';
+    const closeButton = createElement("button");
+    closeButton.textContent = "Close";
+    closeButton.className = "close-button";
+    closeButton.addEventListener("click", app.overlay.closeModal);
 
-    const inputTypes = [
-        'name',
-        'continent',
-        'description',
-        'latitude',
-        'longitude'
-    ];
+    const saveButton = createElement("button");
+    saveButton.textContent = "Save";
+    saveButton.className = "post-button";
 
-
-    createInputFields(inputTypes, modalBox, options, destination);
-
-    const closeButton = createElement('button');
-    closeButton.textContent = 'Close';
-    closeButton.className = 'close-button';
-    closeButton.addEventListener('click', app.overlay.closeModal);
-
-    const saveButton = createElement('button');
-    saveButton.textContent = 'Save';
-    saveButton.className = 'post-button';
-    saveButton.addEventListener('click', () => {
-
-        const name = $('#name-input', modalBox).value;
-        const continent = $('#continent-input', modalBox).value;
-        const description = $('#description-input', modalBox).value;
-        const latitude = parseFloat($('#latitude-input', modalBox).value);
-        const longitude = parseFloat($('#longitude-input', modalBox).value);
-        const position = { latitude, longitude }
-        const dataToSave = { name, continent, description, position, userName: app.userName };
+    saveButton.addEventListener("click", () => {
+        const modalBox = $(".modal-content");
+        const name = $("#name-input", modalBox).value;
+        const continent = $("#continent-input", modalBox).value;
+        const description = $("#description-input", modalBox).value;
+        const latitude = parseFloat($("#latitude-input", modalBox).value);
+        const longitude = parseFloat($("#longitude-input", modalBox).value);
+        const position = { latitude, longitude };
+        const dataToSave = {
+            name,
+            continent,
+            description,
+            position,
+            userName: app.userName
+        };
 
         onSave(dataToSave, destination.id);
     });
-
-    modalBox.appendChild(saveButton);
-    modalBox.appendChild(closeButton);
-
-    app.overlay.$modal.appendChild(modalBox);
+    const buttonContainer = $(".button-container");
+    buttonContainer.appendChild(saveButton);
+    buttonContainer.appendChild(closeButton);
 };
 
 app.overlay = {
-    $modal: $('.modal'),
+    $modal: $(".modal"),
     openAddDestinationForm,
     closeModal,
-    createInputFields
-}
+    prefilledForm
+};
